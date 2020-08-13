e05af25d8c96aae6ea1647ce8e4e701aa0e1637e
Brandon <curriebrc@gmail.com>
2020-08-12 17:05:46 -0700

    added commands to the index.js file

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ client.once("ready", () => {
    console.log(`${client.user.username} is online!`)
 });
 
+client.on("message", async (message) => {
+   const args = message.content.slice(prefix.length).split(' ');
+   const cmd = args.shift().toLowerCase();
+
+   if(cmd === 'ping') {
+      message.reply("pong")
+   }
+})
 client.login(token)
